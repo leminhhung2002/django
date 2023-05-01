c135a37e0ee9e584e956f262b7476d99a4f2da09
leminhhung2002 <96687620+leminhhung2002@users.noreply.github.com>
2023-05-02 00:21:34 +0700

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
+STATIC_URL = '/home/bnb556/django/main/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
